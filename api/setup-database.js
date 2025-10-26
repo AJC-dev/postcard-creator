46c59ac66ce9d570042697652a7024b8ac8249fc
Nauman Mirza <144010824+Nauman-Mirza@users.noreply.github.com>
2025-10-26 16:34:10 +0500

    Update setup-database.js

--- api/setup-database.js
+++ api/setup-database.js
@@ -5,7 +5,7 @@ import fallbackConfig from '../js/config.js';
 // Use environment variables - NO hardcoding!
 const pool = new Pool({
   host: process.env.POSTGRES_HOST,
-  port: 5432,
+  port: 5432, 
   database: process.env.POSTGRES_DATABASE,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
