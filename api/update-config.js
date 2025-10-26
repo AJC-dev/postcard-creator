30aa2d9a57809af5d1d0b3a2713989cf9d9e25e4
Nauman Mirza <144010824+Nauman-Mirza@users.noreply.github.com>
2025-10-26 19:21:49 +0500

    Update update-config.js

--- api/update-config.js
+++ api/update-config.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   port: 5432,
   database: process.env.POSTGRES_DATABASE,
   user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD, 
+  password: process.env.POSTGRES_PASSWORD,  
   ssl: { rejectUnauthorized: false }
 });
 
