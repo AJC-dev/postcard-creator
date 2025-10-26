2dfacf1cf2e494a46be9c65e8882afa2b78b84e5
Nauman Mirza <144010824+Nauman-Mirza@users.noreply.github.com>
2025-10-26 16:34:26 +0500

    Update verify-and-send.js

--- api/verify-and-send.js
+++ api/verify-and-send.js
@@ -8,7 +8,7 @@ const pool = new Pool({
   host: process.env.POSTGRES_HOST,
   port: 5432,
   database: process.env.POSTGRES_DATABASE,
-  user: process.env.POSTGRES_USER,
+  user: process.env.POSTGRES_USER, 
   password: process.env.POSTGRES_PASSWORD,
   ssl: { rejectUnauthorized: false }
 });
