2e55423f24f3f5de03367fe8ed8954c183dec020
Nauman Mirza <144010824+Nauman-Mirza@users.noreply.github.com>
2025-10-27 19:19:53 +0500

    Update setup-database.js

--- api/setup-database.js
+++ api/setup-database.js
@@ -5,7 +5,7 @@ import fallbackConfig from '../js/config.js';
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || process.env.POSTGRES_URL,
   ssl: false,
-  max: 1,
+  max: 1, 
   connectionTimeoutMillis: 10000
 });
 
